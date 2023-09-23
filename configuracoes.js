dc7aa3baf760366c7f2e81944a29fbd5b081d72a
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2023-09-23 19:00:26 -0300

    adicionado uma array com os gifs dos passaros

--- configuracoes.js
+++ configuracoes.js
@@ -1,6 +1,25 @@
 let numeroDeCartas = 1
+let htmlCartas = document.querySelector('section')
+let enderecoCartas = ['gifs/bobrossparrot.gif', 
+                      'gifs/explodyparrot.gif',
+                      'gifs/fiestaparrot.gif', 
+                      'gifs/metalparrot.gif',
+                      'gifs/revertitparrot.gif', 
+                      'gifs/tripletsparrot.gif', 
+                      'gifs/unicornparrot.gif']
 
 // o número precisa ser par e entre 4 e 4
 while (numeroDeCartas < 4 | numeroDeCartas >14 | numeroDeCartas%2 !== 0){
     numeroDeCartas = parseInt(prompt("Insira um número PAR de cartas (entre 4 e 14)"))
 }
+
+
+function gerarCartas(qtCartas){
+    for(let i = 0; i < qtCartas/2; i++){
+        htmlCartas.innerHTML += 
+        `<img src="${enderecoCartas[i]}">
+         <img src="${enderecoCartas[i]}">`
+    }
+}
+
+gerarCartas(numeroDeCartas)
